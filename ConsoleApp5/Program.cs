dd0be4c2f20846455c5f7ea313d346904ad037cb
Danil <d_silchenkov@mail.ru>
2023-09-30 21:24:35 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите координату x: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите координату y: ");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine(((y > 0 || x < 1) && (y < 1 || x <= 1)) && ((y > 0 || x > -1) && (y < 1 || x >= -1)));
